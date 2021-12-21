c9756c93a241b9f4288d4711564a326b9136c4b3
Krishna Prasad <76819369+bharathulaprasad@users.noreply.github.com>
2021-12-21 11:37:38 +0100

    Update Start TC Oracle service.ps1
    give option to start and stop as argument

--- Start TC Oracle service.ps1
+++ Start TC Oracle service.ps1
@@ -1,15 +1,23 @@
 # I tried to simplest way to start all TC services and oracle at one place
 # List of services which needs to be stated
+param ($option)
 $services = @('OracleServiceTC','OracleOraDB19Home1TNSListener','Teamcenter FSC Service FSC_<MACHINE_NAME>_<user>','Teamcenter Process Manager',
-'Teamcenter Server Manager config1_PoolA')
+'Teamcenter Server Manager config1_PoolA','tomcat8')
 
 switch( $services )
 {
     Default
-    {
-        Start-Service -Name $services
+    {	if($option -eq "Stop")
+		{
+			Stop-Service -Name $services
+		}
+		elseif($option -eq "Start")	
+		{
+			Start-Service -Name $services
+		}
+		else
+		{
+			Write-Host "Option is mandatory, please pass -option start or stop"
+		}
     }
 }
-
-#Start tomcat in invisible mode
-Start-Process -FilePath "D:\_my\apps\apache-tomcat-8.5.69\bin\startup.bat" -WindowStyle Hidden
